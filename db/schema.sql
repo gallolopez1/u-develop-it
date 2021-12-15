f47ca4717ec7abc48fb4e1e7b213ac22e9182185
Rooster <gallolopez1@hotmail.com>
2021-12-15 08:54:52 -0500

    Modularize the application

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,11 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
@@ -8,11 +13,6 @@ CREATE TABLE candidates (
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-);
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
 );
 CREATE TABLE voters (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
